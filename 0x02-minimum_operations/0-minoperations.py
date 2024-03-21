8d0110043e84a9ea3b01daa90340892858412c29
siphoprince <mabirimisesipho@gmail.com>
2024-03-21 04:36:17 -0700

    pycode fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,13 +13,12 @@ def minOperations(n):
 
     if n <= 1:
         return 0
-    
+
     dp = [float('inf')] * (n + 1)
     dp[1] = 0
-    
+
     for i in range(2, n + 1):
         for j in range(1, i):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + i // j)
-    
     return dp[n] if dp[n] != float('inf') else 0
