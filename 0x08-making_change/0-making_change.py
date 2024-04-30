40100c2ee717eaa996a292d420994c3f8d59cad4
siphoprince <mabirimisesipho@gmail.com>
2024-04-30 09:34:35 -0700

    trying to fix efficientcy

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,14 +7,13 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    minCoin = [float('inf')] * (total + 1)
-    minCoin[0] = 0
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
 
     for coin in coins:
         for amount in range(coin, total + 1):
-            minCoin[amount] = min(minCoin[amount], minCoin[amount - coin] + 1)
+            min_coins[amount] = min(
+                    min_coins[amount],
+                    min_coins[amount - coin] + 1)
 
-    if minCoin[total] == float('inf'):
-        return -1
-    else:
-        return minCoin[total]
+    return min_coins[total] if min_coins[total] != float('inf') else -1
